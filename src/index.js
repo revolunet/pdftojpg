61a46cecae818913b36b3653a30d4844c076b97d
Julien Bouquillon <julien@revolunet.com>
2017-06-02 14:59:41 +0200

    use streams and gm directly

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
-var fs      = require('fs');
-var path    = require('path');
-var pdf2img = require('pdf2img');
-var express = require('express');
-var bodyParser = require('body-parser')
+var gm = require("gm")
+var express = require("express")
 
-var app = express();
-//var jsonParser = bodyParser.json()
+var app = express()
 
-app.use(bodyParser.raw({ limit: '50mb', type: 'application/pdf' }))
-
-app.post('/convert', function(req, res) {
-
-
-  var pageNumber = 1;
-  var input   = __dirname + path.sep + 'localpdf.pdf';
-  var outputDir = __dirname + path.sep + 'outputfork';
-
-  fs.writeFileSync(input, req.body);
-
-  pdf2img.setOptions({
-    type: 'jpg',
-    page: pageNumber,
-    size: 980,
-    density: 600,
-    outputdir: outputDir,
-    outputname: 'testoutput'
-  });
-
-  var outputPath = outputDir + path.sep + 'testoutput_' + pageNumber + '.jpg'
-
-  pdf2img.convert(input, function(err, info) {
-    if (err) console.log(err)
-    res.writeHead(200, {'Content-Type': 'image/jpeg'});
-    res.end(fs.readFileSync(outputPath));
-  });
-
+app.post("/convert", function(req, res) {
+  console.log("starting PDF->JPG conversion")
+  gm(req).toBuffer("JPG", function(err, buffer) {
+    if (!err) {
+      console.error('ERROR', err);
+      res.status(500).json({
+        error: err.message
+      });
+      return;
+    }
+    console.log("finished PDF->JPG conversion")
+    res.send(buffer)
+  })
 })
 
 app.listen('8888', () => {
