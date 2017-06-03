e3f2efd86f0486fda012fff11203170f984f478f
Julien Bouquillon <julien@revolunet.com>
2017-06-03 23:50:49 +0200

    update with @JonathanMontane work

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
-FROM mhart/alpine-node
-
-# build tools for native dependencies
-RUN apk add --update make gcc g++ python git
-
-# graphicsmagick
-RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories;
-RUN apk add --update graphicsmagick && rm -rf /var/cache/apk/*
-
+FROM jcsousa/nodejs-alpine-graphicsmagick
 
 ENV PORT 1234
-RUN mkdir -p /home/app/src
+
+RUN apk update && apk add ghostscript
+
+RUN mkdir -p /home/app/src/
 WORKDIR /home/app
 COPY package.json /home/app/
 COPY src/ /home/app/src/
--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
+var fs = require("fs")
 var gm = require("gm")
 var express = require("express")
 
 var app = express()
 
-app.post("/convert", function(req, res) {
-  console.log("starting PDF->JPG conversion")
-  gm(req).toBuffer("jpeg", function(err, buffer) {
-    if (err) {
-      console.error('ERROR', err);
-      res.status(500).json({
-        error: err.message
-      });
-      return;
-    }
-    console.log("finished PDF->JPG conversion")
-    res.send(buffer)
+const gmToBuffer = data => new Promise((resolve, reject) => data.stream((err, stdout, stderr) => {
+  if (err) {
+    return reject(err)
+  }
+  const chunks = []
+  stdout.on("data", chunk => {
+    chunks.push(chunk)
   })
+  // these are 'once' because they can and do fire multiple times for multiple errors,
+  // but this is a promise so you'll have to deal with them one at a time
+  stdout.once("end", () => {
+    resolve(Buffer.concat(chunks))
+  })
+  stderr.once("data", data => {
+    reject(String(data))
+  })
+}))
+
+app.post("/convert", (req, res) => {
+  console.log("starting PDF->JPG conversion")
+
+  const pageNumber = 1
+  const size = 1024
+
+  const data = gm(req, `file.pdf[${pageNumber}]`).setFormat("jpeg").resize(size)
+
+  gmToBuffer(data).then(
+    buffer => {
+      console.log("finished PDF->JPG conversion")
+      res.send(buffer)
+    },
+    error => {
+      console.error("ERROR", error)
+      res.status(500).json({
+        error: error.message
+      })
+    }
+  )
 })
 
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || 8888
 
 app.listen(port, () => {
   console.log(`listening on http://127.0.0.1:${port}`)
